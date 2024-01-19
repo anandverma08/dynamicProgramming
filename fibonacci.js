dfbfa1236a2c0bb311c3ec6facb4e85f473692ff
Anand,Vermam <anandverma552@outlook.comm>
2024-01-19 17:06:13 +0530

    climb stairs

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,12 @@
 function helper(n, dp){
     if(n <= 1) return n;
-    if(dp[n] != undefined) return dp[n];   
+    if(n in dp) return dp[n];   
     return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);   
 }
 var fib = function(n) {
-    let dp = [];
+    let dp = {};
     helper(n, dp);
     return dp[n] || n;
 };
+
+console.log(fib(6))
